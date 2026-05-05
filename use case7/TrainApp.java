cc1319b7c37da5f0f846d2661945853bccde5232
acksharavaradhu-creator <acksharavaradhu@gmail.com>
2026-05-05 10:32:15 +0530

    Added UC7 Sorting Bogies using Comparator

---
+++ use case7/TrainApp.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class TrainApp {
+    public static void main(String[] args) {
+        List<Bogie> bogies = new ArrayList<>();
+
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 50));
+        bogies.add(new Bogie("First Class", 30));
+
+        bogies.sort(Comparator.comparingInt(Bogie::getCapacity));
+
+        System.out.println("Sorted Bogies by Capacity:");
+        for (Bogie bogie : bogies) {
+            System.out.println(bogie.getName() + " → " + bogie.getCapacity());
+        }
+    }
+}
